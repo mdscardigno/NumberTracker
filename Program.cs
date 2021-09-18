ccb9120857c14464179ab7097f3f3ae68673f53c
Milagro Scardigno <mdscardigno@gmail.com>
2021-09-17 20:43:56 -0400

    writing and reading to a file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Globalization;
+using CsvHelper;
 
 namespace NumberTracker
 {
@@ -8,6 +11,13 @@ namespace NumberTracker
   {
     static void Main(string[] args)
     {
+      // static void SaveStudents(List<Student> students){
+      //   var writer = new StreamWriter("students.csv");  
+      //   var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+      //   csvWriter.WriterRecords(students);
+      //   writer.Flush();
+      // }
+
       Console.WriteLine("Welcome to Number Tracker");
       //create a list of numbers we will be tracking
       var numbers = new List<int>();
@@ -15,16 +25,17 @@ namespace NumberTracker
       var isRunning = true;
       //while we are running
       while(isRunning){
-        //sow the list of numbers
+        //show the list of numbers
         Console.WriteLine("-------------------------");
         foreach (var number in numbers){
           Console.WriteLine(number);
         }
         Console.WriteLine($"Our list has: {numbers.Count()} entries.");
         Console.WriteLine("-------------------------");
+
         //Ask for a new number or the word quit to end
         Console.WriteLine("Enter a number to store, or to 'quit' to end: ");
-        var input = Console.ReadLine();
+        var input = Console.ReadLine().ToLower();
         if(input == "quit"){
           //if the input is quit, turn off the flag to keep looping
           isRunning = false;
@@ -36,7 +47,25 @@ namespace NumberTracker
             numbers.Add(number);
         }
       }
-    
-    }
-  }
-}
+      //create a stream writing information into a file
+        var fileWriter = new StreamWriter("numbers.csv");
+        //create a file that can write csv to the fileWriter
+        var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
+        //Ask our csvWrite to write out our list of numbers
+        csvWriter.WriteRecords(numbers);
+        //tell the file we are done
+        fileWriter.Close();
+
+      //create a stream reader to get information from our file
+      var fileReader = new StreamReader("numbers.csv");  
+      //tell the csv reader not to interpret the first row as a header, otherwise the first number will be skipped.
+      var config = new CsvConfiguration(CultureInfo.InvariantCulture){
+        //tell the reader not to interpret the first row as a header since it is just a first number.
+        HasHeaderRecord = false,
+      };
+      //create a CSV reader to parse the stream into CSV format
+      var csvReader = new CsvReader(fileReader, config);
+      var numbers = csvReader.GetRecords<int>().ToList();
+    }//end Main
+  }//end Program class
+}//end namespace
